439ef3d69bde3b8e1e9f2a9aaa0253bd31b70823
buckshot307 <buckshot307@gmail.com>
2020-09-28 13:15:23 -0400

    Added custom font, updated html for float, added some js functions

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-//global variables for calculations
+//global variables for display and calculations
+var userDisplay;
 var operator;
 var num1;
 var num2;
+
+//global array for calculator buttons
+var btnArr = [];
+
+
+//global regexp for capturing inputs
+var opRegExp = /\D{1}/g;
+var num1RegExp = /^\d+/g;
+var num1RegExp = /\d+$/g;
 
 //basic operate function for calculating inputs
 const operate = function(operator, num1, num2) {
@@ -19,3 +29,5 @@ const operate = function(operator, num1, num2) {
 document.getElementById("clear").addEventListener("click", function() {
     document.getElementById("display").innerHTML = "";
 });
+
+//function for appending button press to user display
