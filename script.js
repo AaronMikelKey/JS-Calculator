ef8e2f4205ad753a0004c1a72879bcc92a4aa8bc
buckshot307 <buckshot307@gmail.com>
2020-09-29 14:33:01 -0400

    Added a function for the backspace key

--- script.js
+++ script.js
@@ -67,3 +67,21 @@ for (let i=0;i<opInputs.length;i++) {
         }
     }) 
 }
+
+//function for backspace button.  Removes last digit from user display unless the only digit is zero.
+document.getElementById("backspace").addEventListener("click", function() {
+    if (userDisplay != "") { //Shouldn't work after operator key is pressed before another input is added
+        let arr = userDisplay.split(""); //splits user display value into array.
+        console.log(arr + " arr before function");
+        if (arr.length > 1) { //If user has added an input.  Won't work directly after operator key is pressed.
+            arr.pop(); //remove last number added to input
+            userDisplay = arr.join(""); //set userDisplay variable to the modified array
+            document.getElementById("display").querySelector("span").innerHTML = userDisplay; //set user display to userDisplay variable
+        } else if (arr.length = 1 && arr[0] != "0") { //if the only number present is not zero
+            arr[0] = "0"; //set only number to zero
+            userDisplay = arr.join(""); //set userDisplay variable to the modified array (i.e. zero)
+            document.getElementById("display").querySelector("span").innerHTML = userDisplay; //set user display to userDisplay variable
+        }
+    }
+});
+
